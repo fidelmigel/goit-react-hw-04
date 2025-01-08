1c4d1ec19169d56e000d709c787eb912e42c488c
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2025-01-08 19:59:46 +0100

    fixed the errors

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,8 +3,13 @@ import s from "./ImageCard.module.css";
 const ImageCard = ({ photo, onClick }) => {
   const { urls, alt_description } = photo;
   return (
-    <div className={s.wrap} onClick={() => onClick(photo)}>
-      <img className={s.img} src={urls.small} alt={alt_description} />
+    <div className={s.wrap}>
+      <img
+        className={s.img}
+        src={urls.small}
+        alt={alt_description}
+        onClick={() => onClick(photo)}
+      />
     </div>
   );
 };
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,28 +1,35 @@
 import s from "../SearchBar/SearchBar.module.css";
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const SearchBar = ({ onSubmit }) => {
   const [query, setQuery] = useState("");
+
   const handleChange = (e) => {
     setQuery(e.target.value);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!query.trim()) {
-      return alert("Can not be empty");
+      return toast.error("Cannot be empty");
     }
     onSubmit(query);
     setQuery("");
   };
+
   return (
     <div className={s.div}>
+      <Toaster />
       <form className={s.form} onSubmit={handleSubmit}>
         <input
           className={s.input}
-          placeholder="Search..."
+          placeholder="Search images and photos"
           type="text"
           value={query}
           onChange={handleChange}
+          autoComplete="off"
+          autoFocus
         />
         <button type="submit" className={s.btn}>
           Search
